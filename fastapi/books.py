84439970440b90fa0cdcad58d96531dc585d436d
ga1nang <nnthanhmx@gmail.com>
2024-11-20 17:34:37 +0700

    First commit

---
+++ fastapi/books.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, Body
+
+app = FastAPI()
+
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
+
+@app.get("/books")
+async def get_all_books():
+    return BOOKS
+
+@app.get("/books/{book_title}")
+async def get_book_by_title(book_title: str):
+    for book in BOOKS:
+        if book['title'].casefold() == book_title.casefold():
+            return book
+    return {"error": "Book not found"}
+
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+            
+    return books_to_return
+
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and \
+            book.get('category').casefold() == category.casefold():
+                books_to_return.append(book)
+                
+    return books_to_return
+
+
+@app.post("/books/create_book")
+async def create_book(new_book = Body()):
+    BOOKS.append(new_book)
