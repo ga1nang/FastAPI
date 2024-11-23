129322028719e695b798f29d678eaa57e2244aef
ga1nang <nnthanhmx@gmail.com>
2024-11-24 00:31:16 +0700

    Authentication

---
+++ proj3/src/main.py
@@ -0,0 +1,18 @@
+import models
+
+from pydantic import BaseModel
+from fastapi import FastAPI, Depends, HTTPException, Path
+from typing import Annotated
+from sqlalchemy.orm import Session
+from models import Todos, TodoRequest
+from database import engine, SessionLocal
+from starlette import status
+from router import auth, todos
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+app.include_router(auth.router)
+app.include_router(todos.router)
+
---
+++ proj3/src/models.py
@@ -0,0 +1,35 @@
+from database import Base
+from pydantic import BaseModel, Field
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+
+
+class Users(Base):
+    __tablename__ = 'user'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
+
+
+
+class Todos(Base):
+    __tablename__ = 'todos'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(Integer)
+    complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("user.id"))
+    
+    
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
