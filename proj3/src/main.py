b2e098e372f44ec354cb395bfad8ef254639c20d
ga1nang <nnthanhmx@gmail.com>
2024-11-25 18:18:10 +0700

    Authentication

--- proj3/src/main.py
+++ proj3/src/main.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Session
 from models import Todos, TodoRequest
 from database import engine, SessionLocal
 from starlette import status
-from router import auth, todos
+from router import auth, todos, admin, user
 
 app = FastAPI()
 
@@ -15,4 +15,5 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
-
+app.include_router(admin.router)
+app.include_router(user.router)
---
+++ proj3/src/router/user.py
@@ -0,0 +1,66 @@
+from fastapi import APIRouter, Depends, HTTPException, Path
+from typing import Annotated
+from sqlalchemy.orm import Session
+from models import Todos, TodoRequest, Users
+from database import SessionLocal
+from starlette import status
+from pydantic import BaseModel, Field
+from .auth import get_current_user, SECRET_KEY, ALGORITHM, bcrypt_context
+
+
+router = APIRouter(
+    prefix='/user',
+    tags=['user']
+)
+
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+    
+    
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
+
+class UserVerification(BaseModel):
+    old_password: str
+    new_password: str = Field(min_length=6)
+
+
+def authentice_user(usersname: str, password: str, db):
+    user = db.query(Users).filter(Users.username == usersname).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return user
+
+
+@router.get("/", status_code=status.HTTP_200_OK)
+async def get_user(user: user_dependency, db: db_dependency):
+    if user is None or user.get('role') == 'admin':
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    
+    return db.query(Users).filter(Users.id == user.get('id')).first()
+
+
+@router.put("/change_password", status_code=status.HTTP_204_NO_CONTENT)
+async def change_password(user: user_dependency, 
+                          db: db_dependency,
+                          user_verification: UserVerification):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    user_model = db.query(Users).filter(Users.id == user.get('id')).first()
+    
+    if not bcrypt_context.verify(user_verification.old_password, user_model.hashed_password):
+        raise HTTPException(status_code=401, detail='Error on password change.')
+    user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
+    db.add(user_model)
+    db.commit()
+    
+    
+    
